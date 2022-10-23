c87389f3c721e83523c874bee222b28dd185aedb
jvmoreirad <jvitormoreirad@hotmail.com>
2022-10-23 14:56:52 -0300

    feat: delete + patch task routes

--- src/shared/infra/http/routes/tasks.routes.ts
+++ src/shared/infra/http/routes/tasks.routes.ts
@@ -1,13 +1,20 @@
 import { Router } from "express";
 
+import { ChangeTaskStatusController } from "@modules/tasks/useCases/changeTaskStatus/ChangeTaskStatusController";
 import { CreateTaskController } from "@modules/tasks/useCases/createTask/CreateTaskController";
+import { DeleteTaskController } from "@modules/tasks/useCases/deleteTask/DeleteTaskController";
 import { ListTaskByUserController } from "@modules/tasks/useCases/listTaskByUser/ListTasksByUserController";
 
 const createTaskController = new CreateTaskController();
 const listTaskByUserController = new ListTaskByUserController();
+const deleteTaskController = new DeleteTaskController();
+const changeTaskStatusController = new ChangeTaskStatusController();
+
 const tasksRoutes = Router();
 
 tasksRoutes.post("/", createTaskController.handle);
 tasksRoutes.get("/", listTaskByUserController.handle);
+tasksRoutes.delete("/", deleteTaskController.handle);
+tasksRoutes.patch("/", changeTaskStatusController.handle);
 
 export { tasksRoutes };
